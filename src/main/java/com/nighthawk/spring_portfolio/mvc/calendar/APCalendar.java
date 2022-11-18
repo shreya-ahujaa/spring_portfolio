75956c1abb875bc48d21ffbd9051b355d41c14be
shreyaahuja <shreya0519@gmail.com>
2022-11-17 22:05:26 -0800

    finished methods

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/APCalendar.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/APCalendar.java
@@ -1,4 +1,5 @@
 package com.nighthawk.spring_portfolio.mvc.calendar;
+import java.util.*;
 
 // Prototype Implementation
 
@@ -9,22 +10,31 @@ public class APCalendar {
      * isLeapYear(2016) returns True
      */          
     public static boolean isLeapYear(int year) {
-        // implementation not shown
-
-        return false;
+            // implementation not shown
+            if (year % 4 == 0 && year % 100 != 0) {
+                return true;
+            } else if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
         }
-        
+    
     /** Returns the value representing the day of the week 
      * 0 denotes Sunday, 
      * 1 denotes Monday, ..., 
      * 6 denotes Saturday. 
      * firstDayOfYear(2019) returns 2 for Tuesday.
     */
-    private static int firstDayOfYear(int year) {
-        // implementation not shown
+    public static int firstDayOfYear(int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, Calendar.JANUARY);
+        cal.set(Calendar.DAY_OF_MONTH, 1);
 
-        return 0;
-        }
+        // Calendar class returns day of week (Sunday = 1 and Saturday = 7) so subtract 1 to match desired API
+        return cal.get(Calendar.DAY_OF_WEEK) -1;
+    }
 
 
     /** Returns n, where month, day, and year specify the nth day of the year.
@@ -34,27 +44,41 @@ public class APCalendar {
      * dayOfYear(3, 1, 2016) returns 61, since 2016 is a leap year. 
     */ 
     private static int dayOfYear(int month, int day, int year) {
-        // implementation not shown
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month - 1); // Calendar month starts from January = 0 
+        cal.set(Calendar.DAY_OF_MONTH, day);
 
-        return 1;
-        }
+        return cal.get(Calendar.DAY_OF_YEAR);
+    }
+
 
     /** Returns the number of leap years between year1 and year2, inclusive.
      * Precondition: 0 <= year1 <= year2
     */ 
     public static int numberOfLeapYears(int year1, int year2) {
-         // to be implemented in part (a)
-
-        return 0;
+        // to be implemented in part (a)
+        int count = 0;
+        for (int i = year1; i <= year2; i++) {
+            if (isLeapYear(i)) {
+                count++;
+            }
         }
+        return count;
+    }
 
     /** Returns the value representing the day of the week for the given date
      * Precondition: The date represented by month, day, year is a valid date.
     */
     public static int dayOfWeek(int month, int day, int year) { 
-        // to be implemented in part (b)
-        return 0;
-        }
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month - 1); // Calendar month starts from January = 0 
+        cal.set(Calendar.DAY_OF_MONTH, day);
+
+        // Calendar class returns day of week (Sunday = 1 and Saturday = 7) so subtract 1 to match desired API
+        return cal.get(Calendar.DAY_OF_WEEK) -1;
+    }
 
     /** Tester method */
     public static void main(String[] args) {
